edadabbf238355051372870e4612fdce95450516
765564556@qq.com <765564556@qq.com>
2018-11-06 20:14:57 +0800

    'init项目'

---
+++ search-module/src/main/java/com/shop/dev/search/SearchSpringApplication.java
@@ -0,0 +1,11 @@
+package com.shop.dev.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSpringApplication.class);
+    }
+}
